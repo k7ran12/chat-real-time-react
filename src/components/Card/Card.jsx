92088c78f52210fddd0934e5582e8260fdcca05e
Cristian Antonio Valverde <repositorio@fenixideaslab.com>
2024-06-06 15:18:38 -0500

    Probar pipelines

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ const Card = () => {
     <div className="mb-8">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
         <div>
-          <div className="text-xl font-medium text-black">Bienvenido al Chat Git</div>
+          <div className="text-xl font-medium text-black">Bienvenido al Chat</div>
           <p className="text-gray-500">Comunícate con tus amigos.</p>
         </div>
       </div>
